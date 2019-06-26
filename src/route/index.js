2ce3573b8cc49f8693e4cb7c1ed16190fb992298
Yaroslav Melmychenko <yaroslav.melnychenko@chisw.com>
2019-06-26 12:01:25 +0300

    structure and config

---
+++ src/route/index.js
@@ -0,0 +1,16 @@
+import * as urls from '../constants/urls';
+import SignIn from './SignIn';
+// import NotFoundPage from '../components/notFoundPage';
+
+const routers = [
+	{
+		exact: true,
+		path: urls.SIGN_IN,
+		component: SignIn,
+	},
+	// {
+	// 	component: NotFoundPage,
+	// },
+];
+
+export default routers;
