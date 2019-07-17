2feb3dcb7b8be7a3685c63ffdf48e43a649bb9d9
igorrize <igor.lobazoff@gmail.com>
2019-07-17 11:26:39 +0300

    tests index

--- src/components/Tests/Tests.js
+++ src/components/Tests/Tests.js
@@ -2,89 +2,89 @@ import React, { Component } from 'react';
 import { Table, Divider, Tag } from 'antd';
 
 class Tests extends Component {
-  render() {
 
-    const columns = [
-      {
-        title: 'Name',
-        dataIndex: 'name',
-        key: 'name',
-        render: text => text,
-      },
-      {
-        title: 'Age',
-        dataIndex: 'age',
-        key: 'age',
-      },
-      {
-        title: 'Address',
-        dataIndex: 'address',
-        key: 'address',
-      },
-      {
-        title: 'Tags',
-        key: 'tags',
-        dataIndex: 'tags',
-        render: tags => (
-          <span>
-            {tags.map(tag => {
-              let color = tag.length > 5 ? 'geekblue' : 'green';
-              if (tag === 'loser') {
-                color = 'volcano';
-              }
-              return (
-                <Tag color={color} key={tag}>
-                  {tag.toUpperCase()}
-                </Tag>
-              );
-            })}
-          </span>
-        ),
-      },
-      {
-        title: 'Action',
-        key: 'action',
-        render: (text, record) => (
-          <span>
-            Invite {record.name}
-            <Divider type="vertical" />
-            Delete
-          </span>
-        ),
-      },
-    ];
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tests: [],
+    };
+  }
+
+
+  componentDidMount() {
+      fetch('http://10.111.7.3/api/v1/tests', {
+        method: 'get',
+        headers: {Authorization: localStorage.token.replace(/"/g, ''),
+      }})
+
+      .then(response => response.json())
+      .then(data => this.setState({ tests: data }));
+  }
+
+
     
-    const data = [
-      {
-        key: '1',
-        name: 'John Brown',
-        age: 32,
-        address: 'New York No. 1 Lake Park',
-        tags: ['nice', 'developer'],
-      },
-      {
-        key: '2',
-        name: 'Jim Green',
-        age: 42,
-        address: 'London No. 1 Lake Park',
-        tags: ['loser'],
-      },
-      {
-        key: '3',
-        name: 'Joe Black',
-        age: 32,
-        address: 'Sidney No. 1 Lake Park',
-        tags: ['cool', 'teacher'],
-      },
-    ];
+    // const data = [
+    //   {
+    //     key: '1',
+    //     name: 'John Brown',
+    //     age: 32,
+    //     address: 'New York No. 1 Lake Park',
+    //     tags: ['nice', 'developer'],
+    //   },
+    //   {
+    //     key: '2',
+    //     name: 'Jim Green',
+    //     age: 42,
+    //     address: 'London No. 1 Lake Park',
+    //     tags: ['loser'],
+    //   },
+    //   {
+    //     key: '3',
+    //     name: 'Joe Black',
+    //     age: 32,
+    //     address: 'Sidney No. 1 Lake Park',
+    //     tags: ['cool', 'teacher'],
+    //   },
+    // ];
+    render() {
+      const columns = [
+        {
+          title: 'Name',
+          dataIndex: 'name',
+          key: 'name',
+          render: text => text,
+        },
+        {
+          title: 'Description',
+          dataIndex: 'description',
+          key: 'agdescriptione',
+        },
+        {
+          title: 'Questions',
+          dataIndex: 'questions_count',
+          key: 'questions_count',
+        },
+      ];
+  
 
-    return(
+      const { tests, isLoading, error } = this.state;
+  
+      if (error) {
+        return <p>{error.message}</p>;
+      }
+  
+      if (isLoading) {
+        return <p>Loading ...</p>;
+      }
+  
+    return (
       <Table 
         columns={columns} 
-        dataSource={data} 
+        dataSource={tests} 
         pagination={false}
       />
-    )
+    );
   }
 }
 
